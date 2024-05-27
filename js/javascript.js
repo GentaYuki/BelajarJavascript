427b33cd2a9fbded29ea47bf0630d2c9d73ef826
unknown <genta.yuki@gmail.com>
2024-05-27 20:27:15 +0700

    first commit

---
+++ js/javascript.js
@@ -0,0 +1,42 @@
+//luas segitiga//
+function hitungluas() {
+    let a    = document.getElementById("alas").value;
+    let t    = document.getElementById("tinggi").value;
+    let L    = 0.5 * a * t;
+
+    if (a === "" || t === ""){
+        alert('masukan nilai dengan benar')}
+    else if (a <= 0 || t <= 0){
+        alert('nilai tidak boleh minus')
+    }
+    else { document.getElementById("hasilLuas").innerHTML = L;
+    }
+    
+}
+//keliling segitiga//
+function hitungkel() {
+    let sisiA   = document.getElementById("sisiA").value;
+    let sisiB   = document.getElementById("sisiB").value;
+    let sisiC   = document.getElementById("sisiC").value;
+    let kell    = parseInt(sisiA) + parseInt(sisiB) + parseInt(sisiC);
+
+    if (sisiA === "" || sisiB === "" || sisiC === ""){
+        alert('masukan nilai dengan benar')
+    } else if (sisiA <= 0 || sisiB <= 0 || sisiC <= 0){
+        alert('nilai tidak boleh minus')
+    } 
+    else {
+    document.getElementById("hasilKell").innerHTML = kell;
+}}
+ 
+//reset//
+function clearall() {
+    document.getElementById("alas").value = "";
+    document.getElementById("tinggi").value = "";
+    document.getElementById("sisiA").value = "";
+    document.getElementById("sisiB").value = "";
+    document.getElementById("sisiC").value = "";
+    document.getElementById("hasilLuas").innerHTML = "";
+    document.getElementById("hasilKell").innerHTML = "";
+    
+}
